661c9e4e49054cd4e0447210ef41418260ca4a64
Balla Kavya <ballakavya594@gmail.com>
2024-06-25 17:57:27 +0530

    Update

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Post = require('../models/Post');
-const User = require('../models/User');
+const User = require('../models/user');
 const router = express.Router();
 
 router.use((req, res, next) => {
