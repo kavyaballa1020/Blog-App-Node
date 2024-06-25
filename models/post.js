f47b1cb85bb37514ab42d86268d3fd8c8a5249da
Balla Kavya <ballakavya594@gmail.com>
2024-06-25 20:49:22 +0530

    Update

--- models/post.js
+++ models/post.js
@@ -4,7 +4,7 @@ const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  image: { type: String }, // Add this line
+  image: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
