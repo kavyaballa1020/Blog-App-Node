f7bbbe16dd291d6f40cae85cfb4557a14093ce07
Balla Kavya <ballakavya594@gmail.com>
2024-06-25 17:55:27 +0530

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../models/User');
+const User = require('../models/user');
 const router = express.Router();
 
 router.get('/register', (req, res) => {
