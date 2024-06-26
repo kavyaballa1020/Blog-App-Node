6b2267da18e0b656d6bc65752a58a34dea63663b
Balla Kavya <ballakavya594@gmail.com>
2024-06-26 16:21:39 +0530

    "Updated user model to use capital 'U' in mongoose.model, added username uniqueness check in register route, and modified error handling in auth routes. Also, removed trailing whitespace in register.ejs."

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ensureAuthenticated: (req, res, next) => {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect('/auth/login');
-    }
-};
